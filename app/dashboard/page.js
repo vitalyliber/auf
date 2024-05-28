1a9f1ba4da5ae24ca93118ee085750dae7c8f8da
vitaly liber <zenamax@gmail.com>
2024-05-28 20:05:49 +0300

    Add a basic dashboard interface

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,5 @@
 import { db } from "@/db/db.mjs";
-import {fmtDateWithTime} from "@/app/_components/utils";
+import { fmtDateWithTime } from "@/app/_components/utils";
 
 export default async function Dashboard() {
   const users = await db.query.users.findMany({
@@ -15,19 +15,22 @@ export default async function Dashboard() {
         <li>Apps</li>
         <li>Settings</li>
       </ul>
-      <div className="">
+      <div className="w-full">
         <h1 className="text-4xl mb-12">Users</h1>
         <input
           placeholder="Search"
           className="rounded border-gray-300 py-3 min-w-96 mb-8"
         />
 
-        <div className="shadow-sm overflow-hidden my-8">
+        <div className="shadow-sm overflow-hidden my-8 w-full">
           <table className="border-collapse table-auto w-full text-sm">
             <thead>
               <tr>
                 <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
                   Email
+                </th>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                  App
                 </th>
                 <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
                   Created at
@@ -42,6 +45,9 @@ export default async function Dashboard() {
                 <tr key={item.id}>
                   <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
                     {item.email}
+                  </td>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                    Auf.
                   </td>
                   <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
                     {fmtDateWithTime(item.createdAt.toString())}

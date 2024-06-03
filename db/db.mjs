9b2ef8c1dec3c7092d0fda4229936ef927ae53f9
vitaly liber <zenamax@gmail.com>
2024-06-03 12:16:49 +0300

    Change the maximum of connections

--- db/db.mjs
+++ db/db.mjs
@@ -3,5 +3,5 @@ import pg from "pg";
 import * as schema from "./schema.mjs";
 import databaseCredentials from "./databaseCredentials.mjs";
 
-export const connection = new pg.Pool(databaseCredentials);
+export const connection = new pg.Pool({ ...databaseCredentials, max: 20 });
 export const db = drizzle(connection, { schema });

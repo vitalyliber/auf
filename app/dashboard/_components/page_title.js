6df19a2069e42b831806ae07d33b033f89886e2a
vitaly liber <zenamax@gmail.com>
2024-06-05 12:56:47 +0300

    Fix the title styles

--- app/dashboard/_components/page_title.js
+++ app/dashboard/_components/page_title.js
@@ -1,5 +1,5 @@
 import cn from "@/app/(landing)/_components/cn";
 
 export default function PageTitle({ title, classNames }) {
-  return <h1 className={cn("text-4xl mb-12 inline-block", classNames)}>{title}</h1>;
+  return <h1 className={cn("text-4xl mb-12", classNames)}>{title}</h1>;
 }

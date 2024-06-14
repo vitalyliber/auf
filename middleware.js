5b433b208ff53529cf841b0178eb8107122ab8f1
vitaly liber <zenamax@gmail.com>
2024-06-14 13:49:31 +0300

    Redirect the user to a dashboard if he logged in

--- middleware.js
+++ middleware.js
@@ -4,12 +4,11 @@ import { fetchCurrentUser } from "@/auf_next";
 export async function middleware(request) {
   const currentUser = await fetchCurrentUser({ brokenJwtRedirect: false });
 
-  if (!currentUser) {
+  if (currentUser) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/",
 };

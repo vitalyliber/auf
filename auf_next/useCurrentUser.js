216842c4423b800da9f41b1fcd9463647c4c6bf3
vitaly liber <zenamax@gmail.com>
2024-08-01 02:22:39 +0500

    Cache the user between pages

--- auf_next/useCurrentUser.js
+++ auf_next/useCurrentUser.js
@@ -4,18 +4,20 @@ import { fetchCurrentUser } from "./actions";
 import { useStore } from "@nanostores/react";
 import { $user, addUser } from "./store";
 let isFetching = false;
+let cachedUser = null;
 
 export default function useCurrentUser() {
   const user = useStore($user);
 
   const fetchUser = async () => {
-    if (isFetching) return;
+    if (isFetching || cachedUser) return;
     console.log("Fetching the Auf user...");
 
     isFetching = true;
     try {
       const userData = await fetchCurrentUser();
       addUser(userData);
+      cachedUser = userData;
       console.log("The Auf user fetched successfully!");
     } catch (error) {
       console.error("Failed to fetch the Auth user", error);

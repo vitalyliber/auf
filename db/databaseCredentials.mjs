31690f8eb77b30b89a1452a28c0143e1191bfce6
vitaly liber <zenamax@gmail.com>
2024-08-12 16:23:58 +0500

    Upgrade project deps

--- db/databaseCredentials.mjs
+++ db/databaseCredentials.mjs
@@ -8,6 +8,7 @@ const databaseCredentials = process.env.DATABASE_URL
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: process.env.NODE_ENV === "production",
     };
 
 export default databaseCredentials;
